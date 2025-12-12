b9e904f3aaacda2c6dd292d8b613b48eb62292b6
Gulnar Sayyad <gulnar.sayyad@antimsoft.com>
2025-12-12 13:21:51 +0530

    app.py chnges

--- app.py
+++ app.py
@@ -82,5 +82,4 @@ def api_predict():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=8000,debug=True)
